f8dfa95e7bed9870e221e61c7e643525dd90249f
tkmagesh <tkmagesh77@gmail.com>
2014-01-09 15:41:05 -0800

    Web Workers Demo with Reporting

--- Day-1/WebWorkers/workerScript.js
+++ Day-1/WebWorkers/workerScript.js
@@ -1,10 +1,17 @@
 console.log("Message logged from workerscript");
-function someWork(){
-	console.log("some work is executed");
+function doWork(){
+	for(var i=0;i<10000;i++){
+		if (i % 100 === 0){
+			self.postMessage({type : "progress", percentCompleted : i / 100});
+		}
+		for(var j=0;j<1000;j++)
+			for(var k=0;k<1000;k++)
+			{}
+	}	
 }
 self.addEventListener("message",function(msgEvt){
-	if (msgEvt.data === "doWork"){
-		someWork();
-		self.postMessage("completed");
+	if (msgEvt.data.type === "doWork"){
+		doWork();
+		self.postMessage({type : "completed"});
 	}
 })
